8a3d4bcd3bf3c74084a9421119bd03bd21e4fea0
Rubens Leme <rubenss.leme@gmail.com>
2021-04-29 21:49:10 -0300

    Implementar a ação de chamar a tela de login com 2000 mil milesegundos.

---
+++ app/src/main/java/com/example/appmobilebg/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.appmobilebg;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int TEMPO_TELA_ABERTA = 2000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        inizializarTelaDeLogin();
+    }
+    private void inizializarTelaDeLogin(){
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+            finish();
+                Intent abrirTelaSplash = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(abrirTelaSplash);
+            }
+        }, TEMPO_TELA_ABERTA);
+    }
+}
